89dc51b13f24872f7c492dee979ab1396887d90b
JohnnyTurpin <johnnyturpin@mac.com>
2024-11-01 15:50:21 -0400

    Initial Commit

---
+++ CoreAudioInputTesting/CoreAudioInputTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreAudioInputTestingApp.swift
+//  CoreAudioInputTesting
+//
+//  Created by Johnny Turpin on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreAudioInputTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
